e0754b2af831cbcedbaf7846493dc0baf6e6613b
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2023-11-15 14:50:00 -0600

    Update pipelines.py

--- noires/pipelines.py
+++ noires/pipelines.py
@@ -1,13 +1,35 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from pathlib import Path
 
+from scrapy.exporters import CsvItemExporter, JsonItemExporter
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class MultiExporterPipeline:
+    def __init__(self):
+        self.files = {}
+        self.data_directory = Path('data')
 
+    def open_spider(self, spider):
+        csv_file = open(self.data_directory /f'{spider.name}.csv', 'w+b')
+        json_file = open(self.data_directory /f'{spider.name}.json', 'w+b')
+        self.files[spider] = {'csv': csv_file, 'json': json_file}
 
-class NoiresPipeline:
+        # Y del mismo modo para los exporters
+        self.exporters = {
+            'csv': CsvItemExporter(csv_file),
+            'json': JsonItemExporter(json_file),
+        }
+        self.exporters['csv'].start_exporting()
+        self.exporters['json'].start_exporting()
+
+    def close_spider(self, spider):
+        self.exporters['csv'].finish_exporting()
+        self.exporters['json'].finish_exporting()
+        self.files[spider]['csv'].close()
+        self.files[spider]['json'].close()
+
+        # Retira los archivos del diccionario
+        del self.files[spider]
+
     def process_item(self, item, spider):
+        self.exporters['csv'].export_item(item)
+        self.exporters['json'].export_item(item)
         return item
