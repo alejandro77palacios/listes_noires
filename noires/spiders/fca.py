4c919eaad0fcb518c5756f4b90d8e6f4ca289f82
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2023-11-15 14:51:06 -0600

    Clean fca spider

--- noires/spiders/fca.py
+++ noires/spiders/fca.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 import scrapy
 
+from noires.items import FcaLoader
+
 
 class FcaSpider(scrapy.Spider):
     name = "fca"
@@ -10,7 +12,7 @@ class FcaSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.css('td[headers="view-letter-table-column"]>a::attr(href)').getall()
-        self.logger.info(f'Found {len(urls)} urls')
+        self.logger.info(f'Found {len(urls)} urls per page')
         for url in urls:
             yield response.follow(url, callback=self.parse_detail)
         next_page = response.css('a[title="Go to next page"]::attr(href)').get()
@@ -18,19 +20,10 @@ class FcaSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
 
     def parse_detail(self, response):
-        raw_str_date = response.css('span.pubdate.latest>span.date::text').get()
-        if raw_str_date is not None:
-            raw_str_date = raw_str_date.strip()
-            raw_date = datetime.strptime(raw_str_date, '%d/%m/%Y')
-            date = raw_date.strftime('%Y-%m-%d')
-        else:
-            date = ''
-        name = response.css('h1.page-header>span::text').get()
-        website = response.xpath('//p[strong[text()="Website:"]]/text()').get()
-        yield {
-            'name': name.strip() if name is not None else '',
-            'website': website.strip() if website is not None else '',
-            'category': '',
-            'date': date,
-            'source': 'fca',
-        }
+        loader = FcaLoader(response=response)
+        loader.add_css('name', 'h1.page-header>span::text')
+        loader.add_xpath('website', '//p[strong[text()="Website:"]]/text()')
+        loader.add_xpath('category', '//tr[td[1][contains(., "Type")]]/td[2]/a/text()')
+        loader.add_css('date', 'span.pubdate.latest>span.date::text')
+        loader.add_value('source', 'sfc')
+        yield loader.load_item()
