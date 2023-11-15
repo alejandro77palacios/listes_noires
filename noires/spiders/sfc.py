4add382502f4cee1e468addccb543e593f1453a0
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2023-11-15 14:50:56 -0600

    Clean sfc spider

--- noires/spiders/sfc.py
+++ noires/spiders/sfc.py
@@ -1,31 +1,34 @@
-from datetime import datetime
-
 import scrapy
+
+from noires.items import SfcLoader
 
 
 class SfcSpider(scrapy.Spider):
-    name = "sfc"
-    allowed_domains = ["www.sfc.hk"]
-    start_urls = ["https://www.sfc.hk/en/alert-list#full-list"]
+    name = 'sfc'
+    allowed_domains = ['www.sfc.hk']
+    start_urls = ['https://www.sfc.hk/en/alert-list#full-list']
 
     def parse(self, response):
-        urls = response.css('tbody#alert-list-append-here tr>td:nth-child(1)>a::attr(href)').getall()
-        self.logger.info(f'Found {len(urls)} urls')
-        for url in urls:
-            yield response.follow(url, callback=self.parse_detail)
+        companies = response.css('tbody#alert-list-append-here tr>td:nth-child(1)>a')
+        self.logger.info(f'Found {len(companies)} company urls')
+        for company_url in companies:
+            yield response.follow(company_url, callback=self.parse_company)
 
-    def parse_detail(self, response):
-        raw_str_date = response.css('tbody>tr:nth-child(6)>td:nth-child(2)::text').get()
-        if raw_str_date is not None:
-            raw_str_date = raw_str_date.strip()
-            raw_date = datetime.strptime(raw_str_date, '%d %b %Y')
-            date = raw_date.strftime('%Y-%m-%d')
-        else:
-            date = ''
-        yield {
-            'name': response.xpath('//tr[td[1][contains(., "Name")]]/td[2]/text()').get(),
-            'website': response.xpath('//tr[td[1][contains(., "Website")]]/td[2]/text()').get(),
-            'category': response.xpath('//tr[td[1][contains(., "Type")]]/td[2]/a/text()').get(),
-            'date': date,
-            'source': 'sfc',
-        }
+    def parse_company(self, response):
+        loader = SfcLoader(response=response)
+        loader.add_xpath('name', '//tr[td[1][contains(., "Name")]]/td[2]/text()')
+        loader.add_xpath('website', '//tr[td[1][contains(., "Website")]]/td[2]/text()')
+        loader.add_xpath('category', '//tr[td[1][contains(., "Type")]]/td[2]/a/text()')
+        loader.add_css('date', 'tbody>tr:nth-child(6)>td:nth-child(2)::text')
+        loader.add_value('source', 'sfc')
+        yield loader.load_item()
+
+
+if __name__ == "__main__":
+    from scrapy.crawler import CrawlerProcess
+
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+    process.crawl(SfcSpider)
+    process.start()
