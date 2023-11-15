a3748ff069ec632e1be00ce196254605fea3878d
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2023-11-15 14:51:02 -0600

    Clean sec spider

--- noires/spiders/sec.py
+++ noires/spiders/sec.py
@@ -1,37 +1,34 @@
 import scrapy
 
+from noires.items import SecLoader
+
 
 class SecSpider(scrapy.Spider):
     name = "sec"
-    #allowed_domains = ["www.sec.gov"]
+    allowed_domains = ["www.sec.gov"]
     start_urls = ["https://www.sec.gov/enforce/public-alerts"]
 
     def parse(self, response):
-        rows = response.xpath('//tr[contains(@class, "pause-list-page-row")]')
-        print('Total rows:', len(rows))
-        for row in rows:
-            partial_url = row.xpath('./td[1]//a/@href').get().strip()
-            complete_url = response.urljoin(partial_url)
-            yield scrapy.Request(complete_url, callback=self.parse_detail)
-            print(complete_url)
-            # break
+        companies = response.xpath('//tr[contains(@class, "pause-list-page-row")]/td[1]//a')
+        self.logger.info(f'Found {len(companies)} company urls')
+        for company_url in companies:
+            yield response.follow(company_url, callback=self.parse_company)
 
-    def parse_detail(self, response):
-        name = response.css('h1.article-title::text').get()
-        raw_category = response.css('h2::text').get()
-        if raw_category is not None:
-            category = raw_category.replace("[", "").replace("]", "")
-        else:
-            category = raw_category
-        try:
-            text = ' '.join(response.css('p::text').getall())
-            website = text.split('Website')[1].replace(':', '').strip().split()[0]
-        except:
-            website = ''
-        yield {
-            'name': name.strip() if name is not None else '',
-            'website': website.strip(),
-            'category': category.strip() if category is not None else '',
-            'date': '',
-            'source': 'sec',
-        }
+    def parse_company(self, response):
+        loader = SecLoader(response=response)
+        loader.add_css('name', 'h1.article-title::text')
+        loader.add_css('website', 'p::text')
+        loader.add_css('category', 'h2::text')
+        loader.add_value('date', '')
+        loader.add_value('source', 'sec')
+        yield loader.load_item()
+
+
+if __name__ == "__main__":
+    from scrapy.crawler import CrawlerProcess
+
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+    process.crawl(SecSpider)
+    process.start()
